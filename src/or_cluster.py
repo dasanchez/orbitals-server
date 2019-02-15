1ea6ee1dec981c3c7080cd7a6c184c9233922e54
Dante <dante.a.sanchez@gmail.com>
2019-02-15 07:22:57 -0500

    Issue cluster status on new user connection

--- src/or_cluster.py
+++ src/or_cluster.py
@@ -3,6 +3,7 @@ or_cluster.py
 Orbitals cluster module
 Tracks quadrants and routes player packets accordingly.
 """
+import json
 from or_quadrant import OrbitalsQuadrant
 
 quadrantNames = ['ALPHA', 'BETA', 'GAMMA', 'DELTA']
@@ -13,6 +14,8 @@ class OrbitalsCluster:
     def __init__(self, quadrantCount = 4):
         # initialize set of quadrants
         self._quadrants = set()
+        self._users = set()
+        self._userQuadrants = dict()
         self.populateQuadrants(quadrantCount)
         
     def populateQuadrants(self, count):
@@ -27,5 +30,41 @@ class OrbitalsCluster:
         for quadrant in self._quadrants:
             print(f"{quadrant.getQuadrantDetails()}")
 
+    def getClusterStatus(self):
+        # returns a dict with all the quadrants and their players
+        clusterStatus = []
+        for q in self._quadrants:
+            clusterStatus.append(q.getQuadrantDetails())
+        return clusterStatus
+
     # def newPlayer(self, websocket):
         # send quadrant data to new player
+
+    async def newConnection(self, websocket):
+        """ register player """
+        print("New user connected")
+
+        self._users.add(websocket)
+        self._userQuadrants[websocket] = None
+        # issue cluster info
+        packet = sorted(list(self.getClusterStatus()), key=lambda k:k['name'])
+                #   'clusters': {
+                      
+                #   },
+        msg = json.dumps(packet)
+        await websocket.send(msg)
+        # issue list of players
+        # await orbComms.publishPlayers(self._players.getPlayerData(),
+                                    #   self._players.enoughPlayers(), self._users)
+
+    async def deleteConnection(self, websocket):
+        """player has left:
+        1. find out which orbital they belonged to
+        2. send the signal to the relevant orbital
+        """
+
+        if self._userQuadrants[websocket]:
+            q = self._userQuadrants[websocket]
+            print(f"User belonged to {q.getQuadrantDetails()['name']} quadrant")
+        else:
+            print("User did not belong to any quadrant.")
--- src/or_server.py
+++ src/or_server.py
@@ -12,33 +12,35 @@ def main():
     """ starts the game loop """
     print("State set to 'waiting-players'")
     print("Initialized quadrants:")
-    orCluster.printQuadrants()
+    print(f"Quadrants: {orCluster.getClusterStatus()}")
     print("Starting WebSocket server...")
 
 
     # Set up async routines
-    # loop = asyncio.new_event_loop()
-    # asyncio.set_event_loop(loop)
-    # try:
-    #     loop.run_until_complete(websockets.serve(handler, '0.0.0.0', 9001))
-    #     loop.run_forever()
-    # finally:
-    #     loop.close()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(websockets.serve(handler, '0.0.0.0', 9001))
+        loop.run_forever()
+    finally:
+        loop.close()
 
-# async def handle_message(websocket, data):
-#     """ handles incoming message from players """
-#     await orCluster.newMessage(websocket, data)
+async def handle_message(websocket, data):
+    """ handles incoming message from players """
+    pass
+    # await orCluster.newMessage(websocket, data)
 
-# async def handler(websocket, _):
-#     """ register(websocket) sends user_event() to websocket """
-#     await orCluster.newConnection(websocket)
-#     try:
-#         async for message in websocket:
-#             data = json.loads(message)
-#             print(data)
-#             await handle_message(websocket, data)
-#     finally:
-#         await orCluster.deleteConnection(websocket)
+async def handler(websocket, _):
+    """ register(websocket) sends user_event() to websocket """
+    await orCluster.newConnection(websocket)
+    try:
+        async for message in websocket:
+            data = json.loads(message)
+            print(data)
+            await handle_message(websocket, data)
+    finally:
+        # pass
+        await orCluster.deleteConnection(websocket)
 
 if __name__ == "__main__":
     # program entry point
